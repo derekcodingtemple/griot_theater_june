bc1e68000107dbd72c1e79194650ffacd898128b
zaraconsulting <derek@zaraconsulting.org>
2022-06-09 09:24:35 -0500

    update

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,10 @@
 const mobileMenu = document.querySelector( '#mobile-menu' )
+const mobileMenuIcon = document.querySelector( '#mobile-menu-icon' )
 const mobileMenuLinks = document.getElementById( 'mobile-menu-links' )
 
 const handleMobileMenu = () => {
     mobileMenu.classList.toggle( 'change' )
+
     mobileMenuLinks.style.display === 'flex' ?
         mobileMenuLinks.style.display = 'none' :
         mobileMenuLinks.style.display = 'flex'
